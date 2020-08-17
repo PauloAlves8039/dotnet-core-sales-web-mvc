dacaac5f8c314608542e30ba3b34c5df9fa809d8
PauloAlves8039 <pj38alves@gmail.com>
2020-08-17 20:59:42 -0300

    Implementando controlador e view Sellers

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    /// <summary>
+    /// Controlador responsável pelas ações referentes aos vendedores.
+    /// </summary>
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
