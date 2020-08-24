36a4d48d05f217da64776d739dd91d5f8344e21e
PauloAlves8039 <pj38alves@gmail.com>
2020-08-24 12:06:05 -0300

    Tratamento de exceção para delete

---
+++ SalesWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    /// <summary>
+    /// Classe responsável pelo tratamento de exceção relacionada a integridade referencial.
+    /// </summary>
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -55,9 +55,17 @@ namespace SalesWebMvc.Services
         /// <param name="id">parâmetro de remoção do vendedor.</param>
         public async Task RemoveAsync(int id) 
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales - " + e.Message);
+            }
+            
         }
 
         /// <summary>
