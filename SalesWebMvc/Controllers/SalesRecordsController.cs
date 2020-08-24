2198d11256dab6aa0f24f81d6cf05c18d3df7efc
PauloAlves8039 <pj38alves@gmail.com>
2020-08-24 19:54:45 -0300

    Implementação inicial das telas de navegação e pesquisa

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    /// <summary>
+    /// Controlador responsável pela pesquisa dos registros das vendas.
+    /// </summary>
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
